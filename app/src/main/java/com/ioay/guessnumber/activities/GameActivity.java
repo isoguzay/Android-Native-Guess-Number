b96190b9cbb6b393cfa8f9d1420a7509bde87e20
ioay <oguzhan.ay@betek.com.tr>
2019-06-18 17:09:54 +0300

    first commit.

---
+++ app/src/main/java/com/ioay/guessnumber/activities/GameActivity.java
@@ -0,0 +1,73 @@
+package com.ioay.guessnumber.activities;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.ioay.guessnumber.R;
+import com.ioay.guessnumber.fragments.HomeFragment;
+import com.ioay.guessnumber.fragments.ProfileFragment;
+import com.ioay.guessnumber.fragments.SettingsFragment;
+
+public class GameActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new ProfileFragment();
+                    return true;
+                case R.id.navigation_dashboard:
+                    fragment = new HomeFragment();
+                    return true;
+                case R.id.navigation_notifications:
+                    fragment = new SettingsFragment();
+                    return true;
+            }
+            return loadFragment(fragment);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loadFragment(new HomeFragment());
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
---
+++ app/src/main/java/com/ioay/guessnumber/activities/MainActivity.java
@@ -0,0 +1,32 @@
+package com.ioay.guessnumber.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.ioay.guessnumber.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonStart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonStart = findViewById(R.id.button_start);
+
+        buttonStart = findViewById(R.id.button_start);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+    }
+}
